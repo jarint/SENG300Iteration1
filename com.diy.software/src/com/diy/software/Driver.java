dcbda9cff9653f1fc55fef6fe7d6585ab9260cd4
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2022-11-03 22:30:11 -0600

    Update Driver.java

--- com.diy.software/src/com/diy/software/Driver.java
+++ com.diy.software/src/com/diy/software/Driver.java
@@ -1,11 +1,9 @@
 package com.diy.software;
 
 import com.diy.hardware.BarcodedProduct;
-import com.diy.hardware.Product;
 import com.diy.hardware.external.ProductDatabases;
 import com.diy.simulation.Customer;
 import com.diy.simulation.Wallet;
-import com.jimmyselectronics.Item;
 import com.jimmyselectronics.necchi.Barcode;
 import com.jimmyselectronics.necchi.BarcodedItem;
 import com.jimmyselectronics.necchi.Numeral;
@@ -30,9 +28,9 @@ public class Driver
 		BarcodedProduct barcodedProduct1, barcodedProduct2, barcodedProduct3;
 		BarcodedItem barcodedItem1, barcodedItem2, barcodedItem3;
 		
-		double weightOfItem1 = 100;
-		double weightOfItem2 = 200;
-		double weightOfItem3 = 300;
+		double weightOfItem1 = 100.0;
+		double weightOfItem2 = 200.0;
+		double weightOfItem3 = 300.0;
 		
 		long costOfItem1 = 100;
 		long costOfItem2 = 200;
@@ -46,16 +44,14 @@ public class Driver
 		double customer1_maxCreditLimit = 5000;
 		
 	
-		//set up Barcode#1
+		//set up Barcode#1,2,3
 		barcode1 = new Barcode(new Numeral[] { Numeral.one, Numeral.two, Numeral.three, Numeral.four }); // 1234
-		//set up Barcode#2
 		barcode2 = new Barcode(new Numeral[] { Numeral.five, Numeral.six, Numeral.seven, Numeral.eight }); // 5678
 		barcode3 = new Barcode(new Numeral[] { Numeral.three, Numeral.three, Numeral.three, Numeral.three }); // 5678
 		
 		//barcoded produce having barcode1 as barcode, description = "Barcoded Item#1", Cost = 100, weight = 100.0		
 		barcodedProduct1 = new BarcodedProduct(barcode1,"Barcoded Item#1",costOfItem1, weightOfItem1);
-		
-		
+			
 		//barcoded produce having barcode1 as barcode, description = "Barcoded Item#2", Cost = 200, weight = 200.0		
 		barcodedProduct2 = new BarcodedProduct(barcode2,"Barcoded Item#2",costOfItem2, weightOfItem2);
 		
@@ -66,17 +62,13 @@ public class Driver
 		//adding barcoded Item #1 into the database
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode1, barcodedProduct1);		
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode2, barcodedProduct2);
-		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode3, barcodedProduct3);
-		
-		
+		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode3, barcodedProduct3);		
 		//product database is set
 		
-		//create barcoded Items
+		//create barcoded Items (to be used by customer class)
 		barcodedItem1 = new BarcodedItem(barcode1, weightOfItem1);
 		barcodedItem2 = new BarcodedItem(barcode2, weightOfItem2);
 		barcodedItem3 = new BarcodedItem(barcode3, weightOfItem3);
-		
-	
 		
 		//add items into customer's shopping cart
 		customer1.shoppingCart.add(barcodedItem1);
@@ -93,20 +85,13 @@ public class Driver
 		customer1_creditCard.addCardIntoDatabase(customer1_maxCreditLimit);
 		
 		//create the customer wallet;
-		customer1_wallet.cards.add(creditCard);
-		
-		
+		customer1_wallet.cards.add(creditCard);			
 		//Customer created
 		//Credit card set and running (not blocked)
 		
 		System.out.println("no errors");
 		
-		
-		
-		
-		
-
-
+		//customer1 --> Customer Object
+		//customer1_creditCard --> PayByCC.java Object
 	}
-
 }
