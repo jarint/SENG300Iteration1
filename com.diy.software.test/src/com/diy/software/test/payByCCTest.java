5fea2278fac33092ff676db66fb1025cdc960aab
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2022-11-04 21:50:02 -0600

    Update payByCCTest.java

--- com.diy.software.test/src/com/diy/software/test/payByCCTest.java
+++ com.diy.software.test/src/com/diy/software/test/payByCCTest.java
@@ -1,10 +1,9 @@
-/**
- * 
- */
 package com.diy.software.test;
 
 import static org.junit.Assert.*;
 
+import com.jimmyselectronics.opeechee.Card;
+import com.jimmyselectronics.opeechee.InvalidPINException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,17 +18,16 @@ public class PayByCC_test
 {
 	private PayByCC payByCCobject = null;
 	private double creditLimit = 5000;
+	private Card test_card = new Card("Visa", "123456789", "Bob", "123", "1234", true, true);
 
 	/**
+	 * initial setup before every tests includes, set up the credit card, set up the credit card company, add the card into the company database
+	 * 
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception 
 	{
-		//create the object
-		//initalize the credit card
-		//initialize the credit card company
-		//add the card into the company database
 		payByCCobject = new PayByCC();
 		payByCCobject.initialiseCreditCard("Visa", "123456789", "Customer#1", "123", "1234", true, true);
 		payByCCobject.initialiseCreditCardCompany("Credit Card Company # 1", 500);
@@ -37,6 +35,7 @@ public class PayByCC_test
 	}
 
 	/**
+	 * Tear down after every test in this test suite
 	 * @throws java.lang.Exception
 	 */
 	@After
@@ -45,7 +44,10 @@ public class PayByCC_test
 		payByCCobject = null;
 	}
 	
-
+	/**
+	 * Tests the balance in the credit card, since we do not have any transaction posted on the credit card, the current balance will be equal to the credit card limit set, (5000)
+	 * @exceptedResult 5000
+	 */
 	@Test
 	public void Test_balanceNoTransaction() 
 	{
@@ -53,27 +55,45 @@ public class PayByCC_test
 		assertEquals(this.creditLimit, balance, 0.0);
 	}
 	
+	/**
+	 * Tests to see if the transaction is posted or not
+	 * Credit card limit is 5000, and transaction hold amount is 100, transaction amount is 100, so this test should return false
+	 * because we are placing a transaction of 110 while we are only holding 100
+	 * 
+	 * @Excepted_Result False
+	 */
 	@Test
 	public void Test_transaction_bad()
 	{
-		//credit limit is 5000
-		//amount to be put on hold 100
-		//transaction amount 110 --> should result in fail
+		//Transaction amount = 110
+		//Hold Amount = 100
 		boolean result = payByCCobject.postTransaction(110, 100);
 		assertFalse(result);
 	}
 	
+	/**
+	 * Tests to see if the transaction is posted or not
+	 * Credit card limit is 5000, and transaction hold amount is 5000(equal to the credit card limit), transaction amount is 6000, so this test should return false
+	 * because we are placing a transaction of 6000 while we are only holding 5000
+	 * 
+	 * @Excepted_Result False
+	 */
 	@Test
 	public void Test_transaction_bad2()
 	{
-		//credit limit is 5000
-		//amount to be put on hold = credit card
-		//transaction amount 6000 --> should result in fail
-		
+		//Transaction amount = 6000
+		//Hold Amount = 5000
 		boolean result = payByCCobject.postTransaction(6000, payByCCobject.getCreditCardLimit());
 		assertFalse(result);
 	}
 	
+	/**
+	 * Tests to see if the transaction is posted or not
+	 * Credit card limit is 5000, and transaction hold amount is 100, transaction amount is 60, so this test should pass
+	 * because we are placing a transaction of 60 while we are holding 100
+	 * 
+	 * @Excepted_Result True
+	 */
 	@Test
 	public void Test_transaction_good()
 	{
@@ -84,6 +104,11 @@ public class PayByCC_test
 		assertTrue(result);
 	}
 	
+	/**
+	 * Testing the balance of the credit card after the transaction is posted
+	 * 
+	 * @Expected_result  True [4940 (5000 - 60 = 4940]
+	 */
 	@Test
 	public void Test_transaction_remaining_balance()
 	{
@@ -96,20 +121,63 @@ public class PayByCC_test
 		//remaining balance = credit limit, should be equal to 5000 - 60 = 4940
 		assertEquals(4940.00, payByCCobject.getCreditCardLimit(), 0.0);
 	}
+
 	
 	@Test
-	public void Test_getCreditCardData_false()
-	{
-		String invalid_pin = "4567";
-		assertFalse(payByCCobject.getCreditCardData(invalid_pin));
+	public void Test_insertCard() {
+		assertTrue(payByCCobject.insertCard(test_card));
 	}
-	
+
 	@Test
-	public void Test_getCreditCardData_true()
-	{
-		String valid_pin = "1234";
-		assertTrue(payByCCobject.getCreditCardData(valid_pin));
+	public void Test_validateCard1() throws InvalidPINException {
+		assertTrue(payByCCobject.validateCard(test_card, "1234"));
 	}
-	
+
+	@Test
+	public void Test_validateCard2() throws InvalidPINException {
+		assertFalse(payByCCobject.validateCard(test_card, "4321"));
+	}
+
+	@Test
+	public void Test_bankCharge() {
+
+	}
+
+	@Test
+	public void Test_transaction() {
+		double price = 500;
+		assertTrue(payByCCobject.transaction(test_card, price));
+	}
+
+	@Test
+	public void Test_transaction2(){
+		double price = 500000;
+		assertFalse(payByCCobject.transaction(test_card, price));
+	}
+
+	@Test
+	public void Test_transactionResult() {
+		double price = 500;
+		assertTrue(payByCCobject.transactionResult(test_card, price));
+	}
+
+	@Test
+	public void Test_transactionResult2() {
+		double price = 500000;
+		assertFalse(payByCCobject.transactionResult(test_card, price));
+	}
+
+	@Test
+	public void Test_blockCard() {
+		assertFalse(payByCCobject.blockCard(test_card));
+	}
+
+	@Test
+	public void Test_blockCard2() throws InvalidPINException{
+		payByCCobject.validateCard(test_card, "0000");
+		payByCCobject.validateCard(test_card, "1111");
+		payByCCobject.validateCard(test_card, "2222");
+		assertTrue(payByCCobject.blockCard(test_card));
+	}
 
 }
